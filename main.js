10f0b38b36fef07ae13926406a825b790b06f652
Kanjika <97239088+Kanjika@users.noreply.github.com>
2022-10-16 15:31:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(950, 600);
+   canvas.position(700, 150)
+    video = createCapture(VIDEO)
+    video.position(30,150)
+}
